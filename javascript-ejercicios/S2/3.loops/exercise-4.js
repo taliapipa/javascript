a4a404aeccaabf1f891e426ed751658a8a89628f
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-04-28 18:52:12 +0200

    realizado

---
+++ javascript-ejercicios/S2/3.loops/exercise-4.js
@@ -0,0 +1,6 @@
+
+for(let i = 0; i < toys.length; i++){
+    if(toys[i].name.includes("gato")){
+        toys.splice(i, 1);
+    }
+};
---
+++ javascript-ejercicios/S2/3.loops/exercise-5.js
@@ -0,0 +1,7 @@
+
+for(const toy of toys){
+    if(toy.sellCount > 15){
+        popularToys.push(toy)
+    };
+}
+    console.log(popularToys)
